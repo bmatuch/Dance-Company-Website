3130bbdb9cf895d54baeee937531e7dbbf566273
Kaitlyn <Kaitlyn@macbook-pro-5.dhcp.nd.edu>
2023-10-12 15:45:49 -0400

    changes

--- danceco/src/Comoponents/Officers/OfficersList.js
+++ danceco/src/Comoponents/Officers/OfficersList.js
@@ -10,7 +10,7 @@ const OfficerList = ({ officers }) => {
             {officer.get("firstName")} {officer.get("lastName")}
             <br />
             Role: {officer.get("role")}
-            <Collapsible trigger={<button className="button">Learn more about what {officer.get("firstName")} loves about DanceCo!</button>}>
+            <Collapsible trigger={<button className="button">Learn what dances {officer.get("firstName")} is choreographing!</button>}>
               <p>{officer.get("favorite")}</p>
             </Collapsible>
           </li>
