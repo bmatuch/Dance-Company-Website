bcbaecce2f884a0bd486b912b0d3615883378c1e
Kaitlyn <Kaitlyn@macbook-pro-5.dhcp.nd.edu>
2023-10-12 15:17:59 -0400

    updates

---
+++ danceco/src/Comoponents/Officers/OfficersList.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Collapsible from "react-collapsible";
+
+const OfficerList = ({ officers }) => {
+  return (
+    <ul className="list-unstyled">
+      {officers.map((officer) => (
+        <p key={officer.id}>
+          <li>
+            {officer.get("firstName")} {officer.get("lastName")}
+            <br />
+            Role: {officer.get("role")}
+            <Collapsible trigger={<button className="button">Learn more about what {officer.get("firstName")} loves about DanceCo!</button>}>
+              <p>{officer.get("favorite")}</p>
+            </Collapsible>
+          </li>
+        </p>
+      ))}
+    </ul>
+  );
+};
+
+export default OfficerList;
